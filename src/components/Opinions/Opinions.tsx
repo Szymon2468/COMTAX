abe9f5c16da1f1f01e8a4d5e73b90bf66250f220
Szymon Chwila <Szymon_Chwila@MacBook-Pro-Bozena.local>
2022-03-29 12:49:42 +0200

    started making opinions

--- src/components/Opinions/Opinions.tsx
+++ src/components/Opinions/Opinions.tsx
@@ -1,10 +1,36 @@
 import styles from './Opinions.module.scss';
+import logo1 from '../../assets/opinions/Logo1.jpg';
+import logo2 from '../../assets/opinions/Logo2.jpg';
+import logo3 from '../../assets/opinions/Logo3.jpg';
+import logo4 from '../../assets/opinions/Logo4.jpg';
+import logo5 from '../../assets/opinions/Logo5.jpg';
+import Image from 'next/image';
 
 function Opinions() {
+  const logos = [logo1, logo2, logo3, logo4, logo5];
+
+  const generateLogos = () => {
+    let result: JSX.Element[] = [];
+    logos.map((el) => {
+      result.push(<img src={el.src} className={styles.logo} />);
+    });
+    return result;
+  };
+
   return (
     <section>
       <div className={styles.landingPage}>
-        <div className={`container ${styles.homePageContainer}`}></div>
+        <div className={`container`}>
+          <h2 className={styles.title}>
+            TO <span>NASI KLIENCI</span> SĄ DLA NAS <span>NASI KLIENCI</span>
+          </h2>
+          <div className={styles.collabs}>
+            <h3 className={styles.logosTitle}>
+              <span>ONI</span> z nami współpracują
+            </h3>
+            <div className={styles.logos}>{generateLogos()}</div>
+          </div>
+        </div>
       </div>
     </section>
   );
