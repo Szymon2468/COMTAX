2d215b813d702b8ddfadb32bdb94134f9235ea82
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-04-26 20:43:39 +0200

    fix for api url

--- src/lib/httpRequest.ts
+++ src/lib/httpRequest.ts
@@ -1,23 +1,26 @@
 import axios from 'axios';
 
+const API_URL = 'http://localhost:3000/api/';
+
 export const HTTPRequest = async (
   method: 'GET' | 'POST' | 'PUT' | 'DELETE',
   url: string,
   data?: object
 ) => {
   let response;
+  console.log(API_URL);
   switch (method) {
     case 'GET':
-      response = await axios.get(`${process.env.API_URL}/${url}`);
+      response = await axios.get(`${API_URL}/${url}`);
       return response.data;
     case 'POST':
-      response = await axios.post(`${process.env.API_URL}/${url}`, data);
+      response = await axios.post(`${API_URL}/${url}`, data);
       return response.data;
     case 'PUT':
-      response = await axios.put(`${process.env.API_URL}/${url}`, data);
+      response = await axios.put(`${API_URL}/${url}`, data);
       return response.data;
     case 'DELETE':
-      response = await axios.delete(`${process.env.API_URL}/${url}`);
+      response = await axios.delete(`${API_URL}/${url}`);
       return response.data;
     default:
       return null;
