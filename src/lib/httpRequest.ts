6a010c880a87792160bda8d8fd3bf8375bd5f2bf
Jakub Pisula <jakub.pisula@wp.pl>
2022-05-24 23:52:14 +0200

    lol

--- src/lib/httpRequest.ts
+++ src/lib/httpRequest.ts
@@ -20,16 +20,16 @@ export const HTTPRequest = async (
 
     switch (method) {
       case 'GET':
-        response = await axios.get(`${API_URL}/${url}`);
+        response = await axios.get(`/api/${url}`);
         return response.data;
       case 'POST':
-        response = await axios.post(`${API_URL}/${url}`, data);
+        response = await axios.post(`/api/${url}`, data);
         return response.data;
       case 'PUT':
-        response = await axios.put(`${API_URL}/${url}`, data);
+        response = await axios.put(`/api/${url}`, data);
         return response.data;
       case 'DELETE':
-        response = await axios.delete(`${API_URL}/${url}`);
+        response = await axios.delete(`/api/${url}`);
         return response.data;
       default:
         return null;
