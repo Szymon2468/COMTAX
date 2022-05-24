2b878f1e07a361817af186cfca82ce1dfb362490
Jakub Pisula <jakub.pisula@wp.pl>
2022-05-25 01:33:14 +0200

    Implemented hours to reservation form

--- app/lib/cors.js
+++
@@ -1,9 +0,0 @@
-import Cors from 'cors';
-
-const cors = Cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD']
-});
-
-export const handleCors = async (req, res) => {
-  return await cors(req, res);
-};
